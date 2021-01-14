aeb22d63dcf9d0b8623088352edfe5cd580c71a7
Druhin Bhowal <druhin.bhowal@outlook.com>
2021-01-13 21:36:59 -0800

    Fixed some minor issues

--- src/subsys_files/globals.cpp
+++ src/subsys_files/globals.cpp
@@ -5,7 +5,7 @@ pros::Motor LB_drive (2, pros::E_MOTOR_GEARSET_36, false, pros::E_MOTOR_ENCODER_
 pros::Motor RB_drive (3, pros::E_MOTOR_GEARSET_36, false, pros::E_MOTOR_ENCODER_COUNTS);
 pros::Motor RF_drive (4, pros::E_MOTOR_GEARSET_36, false, pros::E_MOTOR_ENCODER_COUNTS);
 pros::Motor vertical_intake_L(5, pros::E_MOTOR_GEARSET_36, false, pros::E_MOTOR_ENCODER_COUNTS);
-pros::Motor vertical_itnake_R(8, pros::E_MOTOR_GEARSET_36, false, pros::E_MOTOR_ENCODER_COUNTS);
+pros::Motor vertical_intake_U(8, pros::E_MOTOR_GEARSET_36, false, pros::E_MOTOR_ENCODER_COUNTS);
 pros::Motor r_horizontal_intake (6, pros::E_MOTOR_GEARSET_36, false, pros::E_MOTOR_ENCODER_COUNTS);
 pros::Motor l_horizontal_intake (7, pros::E_MOTOR_GEARSET_36, false, pros::E_MOTOR_ENCODER_COUNTS);
 
