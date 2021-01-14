49aa2e3070a0cb8ccfa919a563ae04032d598fc9
Druhin Bhowal <druhin.bhowal@outlook.com>
2021-01-13 21:51:50 -0800

    D

--- src/subsys_files/drive.cpp
+++ src/subsys_files/drive.cpp
@@ -4,9 +4,9 @@ pros::ADIGyro gyro('B');
 
 void setDrive(int left, int right)
 {
-  LF_drive = left;
+  //LF_drive = left;
   LB_drive = left;
-  RF_drive = right;
+  //RF_drive = right;
   RB_drive = right;
 }
 
@@ -20,9 +20,9 @@ void resetDriveEncoders()
 
 double averageDriveEncoderValue()
 {
-  return (fabs(//LF_drive.get_position())+
+  return (//fabs(LF_drive.get_position())+
           fabs(LB_drive.get_position())+
-          fabs(//RF_drive.get_position())+
+          //fabs(RF_drive.get_position())+
           fabs(RB_drive.get_position())) / 4;
 }
 //Driver Control Functions
