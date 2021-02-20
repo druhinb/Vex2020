b0ab7b37a923d8baff107c127e610e87f712cc68
druhinb <druhin.bhowal@outlook.com>
2021-02-19 21:16:17 -0800

    a

--- src/main.cpp
+++ src/main.cpp
@@ -67,7 +67,7 @@ void autonomous()
       std::shared_ptr<AsyncMotionProfileController> profileController =
         AsyncMotionProfileControllerBuilder()
           .withLimits({
-            1.0, // Maximum linear velocity of the Chassis in m/s
+            1.5, // Maximum linear velocity of the Chassis in m/s
             1.0, // Maximum linear acceleration of the Chassis in m/s/s
             5.0 // Maximum linear jerk of the Chassis in m/s/s/s
           })
